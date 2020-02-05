97a3265810b3bf5e79a88bcc746edc081c86313f
SelectPerson <test@gmail.com>
2020-02-05 15:46:47 +0200

    More

--- js/script.js
+++ js/script.js
@@ -17,9 +17,46 @@ $(document).ready(() => {
         $('.popup__dipslay').show(200);
         $('.popup__background').css('display','block');
     }
+    /* Scroll */
+
+    let Scroll = document.getElementById('arrow_up');
+    let boolScroll = false;
+    Scroll.style.transition = "opacity 0.5s ease-in";
+    window.addEventListener('scroll', function() {
+        if(window.scrollY >= 1000) {
+            if(!boolScroll) {
+                Scroll.style.display = 'block';
+                setTimeout(function() {
+                    Scroll.style.opacity = '1';
+                    boolScroll = true;
+                },500);
+            }
 
 
+        }
+        else {
+            if(boolScroll == true) {
+                Scroll.style.opacity = '0';
+                setTimeout(function() {
+                    // Scroll.style.display = 'none';
+                    boolScroll = false;
+                },500);
+            }
 
+        }
+    },false);
 
+    Scroll.addEventListener('click', function(e) {
+        console.log('test');
+        if(window.scrollY >= 0) {
+            // this.scrollTo(0,0);
+            // window.scrollY = 2000;
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        }
+        // console.log(window.scrollY);
+    },false);
 
 });
